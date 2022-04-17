d28fc04fa5f6b2523d62edbd3fec25bcd26cdec0
kausarahamed <galibhasan517@gmail.com>
2022-04-17 15:43:10 +0600

    card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Package from "../Package/Package";
 import Banner from "./Banner";
 
 const Home = () => {
+  const [packags, setpackages] = useState([]);
+  useEffect(() => {
+    fetch("fakedata.json")
+      .then((response) => response.json())
+      .then((data) => setpackages(data));
+  }, []);
   return (
     <div>
       <Banner></Banner>
+      <div className="pt-10 grid grid-cols-3 gap-5 py10">
+        {packags.map((packag) => (
+          <Package key={packag.id} packag={packag}></Package>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Package/Package.js
+++ src/components/Package/Package.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const Package = () => {
-  return <div>This is package</div>;
+const Package = ({ packag }) => {
+  const navigate = useNavigate;
+  const { name, price, img, description } = packag;
+  return (
+    <div>
+      <div className="mx-5 mb-10 bg-gray-200 rounded-xl h-[650px] relative">
+        <img className="rounded-t-xl w-full mt-3 h-[200px]" src={img} alt="" />
+        <h3 className="text-2xl font-serif italic text-center text-blue-500 font-bold pt-1">
+          {name}
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Package;
