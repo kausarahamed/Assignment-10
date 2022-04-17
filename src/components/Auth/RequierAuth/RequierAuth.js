854e96ff447f580a155ab625ac134ddac76a4d06
kausarahamed <galibhasan517@gmail.com>
2022-04-17 11:43:16 +0600

    hook problem solve

--- src/components/Auth/RequierAuth/RequierAuth.js
+++ src/components/Auth/RequierAuth/RequierAuth.js
@@ -1,10 +1,9 @@
-// import { auth } from "../../firebase.init";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
-import auth from "../../firebase.init";
+import { auth } from "../../firebase.init";
 
 const RequierAuth = ({ childern }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading, error] = useAuthState(auth);
   const location = useLocation();
   if (user) {
     return childern;
--- src/components/firebase.init.js
+++ src/components/firebase.init.js
@@ -17,5 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-export default auth;
+export const auth = getAuth(app);
+export default app;
