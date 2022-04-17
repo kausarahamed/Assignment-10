889822bacbaa310c644e75daa4eba628d425893b
kausarahamed <galibhasan517@gmail.com>
2022-04-17 14:49:33 +0600

    slideer compleate

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,9 +1,35 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import banner1 from "../../img/home.webp";
+import banner2 from "../../img/group.jpg";
+import banner3 from "../../img/online.jpg";
 
 const Banner = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+  };
   return (
-    <div>
-      <h2>banner</h2>
+    <div className="mx-8 mb-8  ">
+      <Slider {...settings}>
+        <div>
+          <img className="h-[500px] w-full" src={banner1} alt="" />
+        </div>
+        <div>
+          <img className="h-[500px] w-full" src={banner2} alt="" />
+        </div>
+        <div>
+          <img className="h-[500px] w-full" src={banner3} alt="" />
+        </div>
+      </Slider>
     </div>
   );
 };
